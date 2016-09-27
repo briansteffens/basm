208f5736838d2fa61851f46a48682408e7c4b516
Brian Steffens <briansteffens@gmail.com>
2016-09-27 14:07:22 -0400

    Parse command and operands

--- Main.hs
+++ Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
 import Data.List
+import Data.Binary.Put
 import qualified Data.Text as T
+import qualified Data.ByteString.Lazy as BL
 
 data Section = Section {
     kind :: T.Text,
@@ -10,8 +12,22 @@ data Section = Section {
 
 data Instruction = Instruction {
     labels :: [T.Text],
-    source :: T.Text
+    source :: T.Text,
+    command :: T.Text,
+    operands :: [T.Text]
 }
+
+assembledLength :: Instruction -> Int
+assembledLength inst = 8
+
+parseInstruction :: [T.Text] -> T.Text -> Instruction
+parseInstruction labels source = do
+    let parts = T.break (== ' ') source
+    let command = fst parts
+    let operands = [o | o <- map T.strip (T.split (== ',') (snd parts)),
+                    not (T.null o)]
+
+    Instruction labels source command operands
 
 parseInstructions [] = []
 parseInstructions lines = do
@@ -29,7 +45,7 @@ parseInstructions lines = do
     let source = if noLabel then inst
                  else (T.strip (T.drop 1 (snd labelParts)))
 
-    [Instruction labels source] ++ parseInstructions (drop 1 (snd parts))
+    [parseInstruction labels source] ++ parseInstructions (drop 1 (snd parts))
 
 parseSections kind lines = do
     let startsWithSection = \s -> (T.take 9 s) == (T.pack "section .")
@@ -47,12 +63,21 @@ showSection s = do
 showInstruction :: Instruction -> IO ()
 showInstruction i = do
     putStr (T.unpack (T.intercalate (T.pack ",") (labels i)))
-    putStr (if (null (labels i)) then "" else "> ")
-    putStrLn (T.unpack (source i))
+    putStr (if (null (labels i)) then "" else ">")
+    putStr (T.unpack (command i))
+    putStr (if (null (operands i)) then "" else "|")
+    putStr (T.unpack (T.intercalate (T.pack ",") (operands i)))
+    putStr "\n"
 
 trimLines x = map T.strip x
 removeBlankLines x = [y | y <- x, not (T.null y)]
 removeComments x = [y | y <- x, (T.head) y /= ';']
+
+assemble :: Put
+assemble = do
+  putWord32be 1
+  putWord16be 2
+  putWord8 3
 
 main :: IO ()
 main = do
@@ -74,3 +99,5 @@ main = do
     putStr "\n"
     showSection (sections !! 2)
     putStr "\n"
+
+    --BL.putStr $ runPut assemble
