cd0720a8fc526d9dcfdc249ae485a4a24ddb76d8
Brian Steffens <briansteffens@gmail.com>
2016-12-19 20:54:57 -0500

    Improve compare script

--- research/compare.py
+++ research/compare.py
@@ -20,14 +20,18 @@ def assemble(instruction):
         return f.read()[0x180:][:size]
 
 
-registers = [
-    'rax', 'rbx', 'rcx', 'rdx', 'rdi', 'rsi', 'rbp', 'rsp',
-    'r8',  'r9',  'r10', 'r11', 'r12', 'r13', 'r14', 'r15',
-    'eax', 'ebx', 'ecx', 'edx', 'edi', 'esi', 'ebp', 'esp',
-    'ax',  'bx',  'cx',  'dx',  'di',  'si',  'bp',  'sp',
-    'al',  'bl',  'cl',  'dl',
-    'ah',  'bh',  'ch',  'dh',
-]
+regs64s = ['rax', 'rbx', 'rcx', 'rdx', 'rdi', 'rsi', 'rbp', 'rsp']
+regs64x = ['r8',  'r9',  'r10', 'r11', 'r12', 'r13', 'r14', 'r15']
+regs32  = ['eax', 'ebx', 'ecx', 'edx', 'edi', 'esi', 'ebp', 'esp']
+regs16  = ['ax',  'bx',  'cx',  'dx',  'di',  'si',  'bp',  'sp' ]
+regs8l  = ['al',  'bl',  'cl',  'dl']
+regs8h  = ['ah',  'bh',  'ch',  'dh']
+
+regs64 = regs64s + regs64x
+regs8 = regs8l + regs8h
+
+regs = regs64 + regs32 + regs16 + regs8
+
 
 class Instruction(object):
     def __init__(self, command, operands):
@@ -38,12 +42,28 @@ class Instruction(object):
         self.hex = None
         self.bin = None
 
+
+INST_DIVIDER = Instruction(None, None)
+
 instructions = []
 
-for reg in registers:
-    instructions.append(Instruction('inc', [reg]))
+for regset in [regs64, regs32, regs16, regs8]:
+    for cmd in ['cmp', 'mov']:
+        for reg in regset:
+            instructions.append(Instruction(cmd, [reg, '7']))
+        instructions.append(INST_DIVIDER)
+
+for regset in [regs64, regs32, regs16, regs8]:
+    for cmd in ['inc', 'dec']:
+        for reg in regset:
+            instructions.append(Instruction(cmd, [reg]))
+        instructions.append(INST_DIVIDER)
+
 
 for inst in instructions:
+    if inst == INST_DIVIDER:
+        continue
+
     inst.instruction = inst.command
     if len(inst.operands) > 0:
         inst.instruction += ' ' + ', '.join(inst.operands)
@@ -53,8 +73,10 @@ for inst in instructions:
     inst.hex = [hex(a)[2:].zfill(2) for a in inst.assembled]
     inst.bin = [bin(a)[2:].zfill(8) for a in inst.assembled]
 
-instruction_max = max([len(i.instruction) for i in instructions])
-bytes_max = max([len(i.assembled) for i in instructions])
+non_special = [i for i in instructions if i != INST_DIVIDER]
+
+instruction_max = max([len(i.instruction) for i in non_special])
+bytes_max = max([len(i.assembled) for i in non_special])
 hex_max = 3 * bytes_max - 1
 
 def pad(string, length):
@@ -64,6 +86,10 @@ def pad(string, length):
     return string
 
 for inst in instructions:
+    if inst == INST_DIVIDER:
+        print()
+        continue
+
     instruction = pad(inst.instruction, instruction_max)
     hex_out = pad(' '.join(inst.hex), hex_max)
     bin_out = ' '.join(inst.bin)
