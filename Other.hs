ccbf99699d6ece552773ca890bc92c5bd29d8a7d
Brian Steffens <briansteffens@gmail.com>
2017-01-03 02:02:45 -0500

    Generate ModR/M byte

--- Other.hs
+++ Other.hs
@@ -21,6 +21,7 @@ data Registers = NoRegister
                | R8D  | R9D  | R10D | R11D | R12D | R13D | R14D | R15D
                | R8W  | R9W  | R10W | R11W | R12W | R13W | R14W | R15W
                | R8B  | R9B  | R10B | R11B | R12B | R13B | R14B | R15B
+               | RIP  | EIP
                deriving Eq
 
 registers8 = [AL, BL, CL, DL,
@@ -28,21 +29,19 @@ registers8 = [AL, BL, CL, DL,
               BPL, SPL, SIL, DIL,
               R8B, R9B, R10B, R11B, R12B, R13B, R14B, R15B]
 
-data Displacement = Displacement8  Int
-                  | Displacement16 Int
+extendedRegisters = [R8,  R9,  R10,  R11,  R12,  R13,  R14,  R15,
+                     R8D, R9D, R10D, R11D, R12D, R13D, R14D, R15D,
+                     R8W, R9W, R10W, R11W, R12W, R13W, R14W, R15W,
+                     R8B, R9B, R10B, R11B, R12B, R13B, R14B, R15B]
+
+data Displacement = NoDisplacement
+                  | Displacement8  Int
                   | Displacement32 Int
 
-data Scale = Scale1
+data Scale = NoScale
            | Scale2
            | Scale4
            | Scale8
-
-data AddressOperand = AddressOperand {
-    base         :: Registers,
-    index        :: Registers,
-    scale        :: Scale,
-    displacement :: Displacement
-}
 
 data Operand = Register  Registers
              | Address   Registers Scale Registers Displacement
@@ -55,22 +54,98 @@ data Instruction = Instruction {
     operands :: [Operand]
 }
 
+
+-- Get the least significant 3 bits for a register.
+registerIndex :: Registers -> [Int]
+registerIndex r
+    | elem r [AL, AX, EAX, RAX, R8B, R8W, R8D, R8]          = [0, 0, 0]
+    | elem r [CL, CX, ECX, RCX, R9B, R9W, R9D, R9]          = [0, 0, 1]
+    | elem r [DL, DX, EDX, RDX, R10B, R10W, R10D, R10]      = [0, 1, 0]
+    | elem r [BL, BX, EBX, RBX, R11B, R11W, R11D, R11]      = [0, 1, 1]
+    | elem r [AH, SPL, SP, ESP, RSP, R12B, R12W, R12D, R12] = [1, 0, 0]
+    | elem r [CH, BPL, BP, EBP, RBP, R13B, R13W, R13D, R13] = [1, 0, 1]
+    | elem r [DH, SIL, SI, ESI, RSI, R14B, R14W, R14D, R14] = [1, 1, 0]
+    | elem r [BH, DIL, DI, EDI, RDI, R15B, R15W, R15D, R15] = [1, 1, 1]
+
+
+-- The REX byte extension bit is 1 for extended registers and 0 for standard.
+rexBit :: Registers -> Int
+rexBit r
+    | elem r extendedRegisters = 1
+    | otherwise                = 0
+
+
+-- Get the ModR/M bits for the given register. Returns 4 bits. The first bit is
+-- the REX portion of the encoding. The last 3 bits are the ModR/M portion.
+encodeRegister :: Registers -> [Int]
+encodeRegister r = [rexBit r] ++ registerIndex r
+
+
+-- Get the operand directionality for an opcode. Controls the order in which
+-- operands are encoded in the ModR/M byte. 0 is normal, 1 is reversed.
+directionality :: Int -> Int
+directionality op
+    | elem op [0x02, 0x03] = 1
+    | otherwise            = 0
+
+
+-- Reorder operands based on opcode directionality (flipping them if needed).
+encodedOrder :: Int -> [Operand] -> [Operand]
+encodedOrder op operands
+    | directionality op == 1 = reverse operands
+    | otherwise              = operands
+
+
+-- Generate the first two bits of ModR/M field.
+-- Operands must be in encoded order.
+modBits :: [Operand] -> [Int]
+modBits [Address  _   _ _  NoDisplacement   , _] = [0, 0]
+modBits [Address  RIP _ _ (Displacement32 _), _] = [0, 0]
+modBits [Address  EIP _ _ (Displacement32 _), _] = [0, 0]
+modBits [Address  _   _ _ (Displacement8  _), _] = [0, 1]
+modBits [Address  _   _ _ (Displacement32 _), _] = [1, 0]
+modBits [Register _                         , _] = [1, 1]
+
+
+-- Generate the R/M bits of a ModR/M byte.
+rmBits :: Operand -> [Int]
+rmBits (Register r                                        ) = registerIndex r
+rmBits (Address  RIP NoScale NoRegister (Displacement32 _)) = [1, 0, 1]
+rmBits (Address  EIP NoScale NoRegister (Displacement32 _)) = [1, 0, 1]
+rmBits (Address  r   NoScale NoRegister _                 ) = registerIndex r
+rmBits (Address  _   _       _          _                 ) = [1, 0, 0] -- SIB
+
+
+-- Generate the REG bits of a ModR/M byte.
+regBits :: Operand -> [Int]
+regBits (Register r) = registerIndex r
+regBits _            = [0, 0, 0]
+
+
+-- Generate a ModR/M byte.
+-- Operands must be in encoded order.
+modRmByte :: [Operand] -> [Int]
+modRmByte [op1, op2] = modBits [op1, op2] ++ regBits op2 ++ rmBits op1
+
+
+-- Get an opcode based on an assembly command and its operators.
 opcode :: Command -> [Operand] -> Int
 opcode ADD [_,               Register r]
-    | elem r registers8                       = 0x00
-    | otherwise                               = 0x01
+    | elem r registers8                       = 0x00 -- r8, r
+    | otherwise                               = 0x01 -- *, r
 opcode ADD [Register r,      Address _ _ _ _]
-    | elem r registers8                       = 0x02
-    | otherwise                               = 0x03
-opcode ADD [Register AL,     Immediate _]     = 0x04
-opcode ADD [Register AX,     Immediate _]     = 0x05
-opcode ADD [Register EAX,    Immediate _]     = 0x05
-opcode ADD [Register RAX,    Immediate _]     = 0x05
-opcode ADD [Address _ _ _ _, Immediate [_]]   = 0x80
+    | elem r registers8                       = 0x02 -- r8, addr
+    | otherwise                               = 0x03 -- r!8, addr
+opcode ADD [Register AL,     Immediate _]     = 0x04 -- AL, imm
+opcode ADD [Register AX,     Immediate _]     = 0x05 -- AX, imm
+opcode ADD [Register EAX,    Immediate _]     = 0x05 -- EAX, imm
+opcode ADD [Register RAX,    Immediate _]     = 0x05 -- RAX, imm
+opcode ADD [Address _ _ _ _, Immediate [_]]   = 0x80 -- addr, imm8
 opcode ADD [Register r,      Immediate [_]]
-    | elem r registers8                       = 0x80
-    | otherwise                               = 0x83
-opcode ADD [_,               Immediate _]     = 0x81
+    | elem r registers8                       = 0x80 -- r8, imm8
+    | otherwise                               = 0x83 -- r!8, imm8
+opcode ADD [_,               Immediate _]     = 0x81 -- *, imm
+
 
 main :: IO ()
 main = do
@@ -89,7 +164,7 @@ main = do
              labels = [],
              command = ADD,
              operands = [
-                 Address RAX Scale1 NoRegister (Displacement8 0),
+                 Address RAX NoScale NoRegister NoDisplacement,
                  Register BL
              ]
          },
@@ -107,7 +182,7 @@ main = do
              labels = [],
              command = ADD,
              operands = [
-                 Address RAX Scale1 NoRegister (Displacement8 0),
+                 Address RAX NoScale NoRegister NoDisplacement,
                  Register RBX
              ]
          },
@@ -117,7 +192,7 @@ main = do
              command = ADD,
              operands = [
                  Register BL,
-                 Address RAX Scale1 NoRegister (Displacement8 0)
+                 Address RAX NoScale NoRegister NoDisplacement
              ]
          },
          Instruction {
@@ -126,7 +201,7 @@ main = do
              command = ADD,
              operands = [
                  Register RBX,
-                 Address RAX Scale1 NoRegister (Displacement8 0)
+                 Address RAX NoScale NoRegister NoDisplacement
              ]
          },
          Instruction {
@@ -161,7 +236,7 @@ main = do
              labels = [],
              command = ADD,
              operands = [
-                 Address RCX Scale8 RDX (Displacement8 456),
+                 Address RCX Scale8 RDX (Displacement32 456),
                  Immediate [0x01, 0xc8]
              ]
          },
@@ -175,7 +250,11 @@ main = do
              ]
          }]
 
-    let go i = (showHex (opcode (command i) (operands i)) "\t") ++
-               (source i)
+    let go i = do
+        let op = opcode (command i) (operands i)
+        (showHex op " ") ++
+            (show (modRmByte (encodedOrder op (operands i)))) ++
+            "\t" ++
+            (source i)
 
     putStrLn (intercalate "\n" (map go instructions))
