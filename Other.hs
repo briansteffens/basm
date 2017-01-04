f1b1b8de4e2efda291b196079be60a7ff7eec1a3
Brian Steffens <briansteffens@gmail.com>
2017-01-04 09:14:26 -0500

    Add size and REX prefixes

--- Other.hs
+++ Other.hs
@@ -3,13 +3,24 @@ module Main where
 import Data.List
 import qualified Data.ByteString as B
 import Numeric (showHex)
+import Debug.Trace (trace)
+
+
+data Result a = Success a
+              | Warn String a
+              | Fail String
+
 
 data Command = ADD
+
 
 data Size = BYTE
           | WORD
           | DWORD
           | QWORD
+          | NoSize
+          deriving (Eq, Show)
+
 
 data Registers = NoRegister
                | RAX  | RBX  | RCX  | RDX  | RBP  | RSP  | RSI  | RDI
@@ -24,35 +35,175 @@ data Registers = NoRegister
                | RIP  | EIP
                deriving Eq
 
-registers8 = [AL, BL, CL, DL,
-              AH, BH, CH, DH,
+
+registersHigh8 = [AH, BH, CH, DH]
+
+
+registers8 = registersHigh8 ++
+             [AL, BL, CL, DL,
               BPL, SPL, SIL, DIL,
               R8B, R9B, R10B, R11B, R12B, R13B, R14B, R15B]
+
+
+registers16 = [AX, BX, CX, DX, BP, SP, SI, DI,
+               R8W, R9W, R10W, R11W, R12W, R13W, R14W, R15W]
+
 
 extendedRegisters = [R8,  R9,  R10,  R11,  R12,  R13,  R14,  R15,
                      R8D, R9D, R10D, R11D, R12D, R13D, R14D, R15D,
                      R8W, R9W, R10W, R11W, R12W, R13W, R14W, R15W,
                      R8B, R9B, R10B, R11B, R12B, R13B, R14B, R15B]
 
+
+registers64 = [RAX, RBX, RCX, RDX, RBP, RSP, RSI, RDI, RIP] ++
+              extendedRegisters
+
+
 data Displacement = NoDisplacement
                   | Displacement8  Int
                   | Displacement32 Int
+
 
 data Scale = NoScale
            | Scale2
            | Scale4
            | Scale8
 
+
 data Operand = Register  Registers
              | Address   Registers Scale Registers Displacement
              | Immediate [Int]
 
+
 data Instruction = Instruction {
     source   :: String,
     labels   :: [String],
+    sizeHint :: Size,
     command  :: Command,
     operands :: [Operand]
 }
+
+
+-- Opcodes that default to 32-bit operation size without any prefix bytes
+ops32 = [0x01, 0x03, 0x05, 0x81, 0x83]
+
+
+-- Check if an operand references any of a list of registers
+anyRegisters :: Operand -> [Registers] -> Bool
+anyRegisters (Immediate _        ) _    = False
+anyRegisters (Register  r        ) regs = elem r  regs
+anyRegisters (Address   r1 _ r2 _) regs = elem r1 regs || elem r2 regs
+
+
+-- Check if an operand references any extended registers
+anyExtendedRegisters :: Operand -> Bool
+anyExtendedRegisters operand = anyRegisters operand extendedRegisters
+
+
+-- Check if an operand references any high-byte registers
+anyHigh8Registers :: Operand -> Bool
+anyHigh8Registers operand = anyRegisters operand registersHigh8
+
+
+-- The optional extension bit for encoding a register.
+exBit :: Registers -> Int
+exBit r = if elem r extendedRegisters then 1 else 0
+
+
+-- Generate the extension bit of the MODRM.reg field.
+-- Operands must be in encoded order.
+extensionR :: [Operand] -> Int
+extensionR [_, Register r] = exBit r
+extensionR [_, _         ] = 0
+
+
+-- Generate the extension bit of the SIB.index field.
+-- Operands must be in encoded order.
+extensionX :: [Operand] -> Int
+extensionX [Address _ _ r _, _] = exBit r
+extensionX [_              , _] = 0
+
+
+-- Generate the extension bit of the MODRM.rm or SIB.base field.
+-- Operands must be in encoded order.
+extensionB :: [Operand] -> Int
+extensionB [Register r      , _] = exBit r
+extensionB [Address  r _ _ _, _] = exBit r
+extensionB [_               , _] = 0
+
+
+-- Encode a REX byte if necessary.
+-- Operands must be in encoded order.
+-- TODO: Make sure extended registers and high byte registers aren't mixed
+rex :: Int -> Size -> [Operand] -> [Int]
+rex op size [op1, op2]
+    | (size == QWORD && elem op ops32) ||
+      anyExtendedRegisters op1 ||
+      anyExtendedRegisters op2 = [0, 1, 0, 0,
+                                  if size == QWORD then 1 else 0,
+                                  extensionR [op1, op2],
+                                  extensionX [op1, op2],
+                                  extensionB [op1, op2]]
+    | otherwise                = []
+
+
+-- Encode a size override prefix if necessary.
+prefixSize16 :: Int -> Size -> [Int]
+prefixSize16 op size
+    | elem op ops32 && size == WORD = [0x66]
+    | otherwise                     = []
+
+
+-- Encode any necessary sizing prefixes for an instruction
+-- Operands must be in encoded order.
+sizePrefix :: Int -> Size -> [Operand] -> [Int]
+sizePrefix op size operands = (prefixSize16 op size) ++
+                              (rex op size operands)
+
+
+-- Get the size/width of a register
+registerSize :: Registers -> Size
+registerSize r
+    | elem r registers8  = BYTE
+    | elem r registers16 = WORD
+    | elem r registers64 = QWORD
+    | otherwise          = DWORD
+
+
+-- Get the size/width of an operand
+operandSize :: Operand -> Maybe Size
+operandSize (Register r         ) = Just (registerSize r)
+operandSize (Immediate [_]      ) = Just BYTE             -- imm8
+operandSize (Immediate [_,_,_,_]) = Just DWORD            -- imm32
+operandSize (Address    _ _ _ _ ) = Nothing
+
+
+-- Infer the operation size based on its operands, if possible.
+inferOpSize :: [Operand] -> Result Size
+inferOpSize [Address _ _ _ _, Immediate _    ] = Success NoSize
+inferOpSize [Register r     , Immediate _    ] = Success (registerSize r)
+inferOpSize [Address _ _ _ _, Register r     ] = Success (registerSize r)
+inferOpSize [Register r     , Address _ _ _ _] = Success (registerSize r)
+inferOpSize [Register r1    , Register r2    ]
+    | size1 == size2                           = Success size1
+    | otherwise                                = Fail "Operand size mismatch"
+    where size1 = registerSize r1
+          size2 = registerSize r2
+
+
+-- Given an operation size and a size hint, work out which takes precedence.
+applySizeHint :: Result Size -> Size -> Result Size
+applySizeHint (Success NoSize) NoSize      = Fail "Missing operand size"
+applySizeHint (Success NoSize) hint        = Success hint
+applySizeHint (Success size)   NoSize      = Success size
+applySizeHint (Success size)   hint
+    | size == hint                         = Success size
+    | otherwise                            = Warn "Size hint ignored" size
+
+
+-- Sort out an operation size given its operands and size hint.
+opSize :: Size -> [Operand] -> Result Size
+opSize hint operands = applySizeHint (inferOpSize operands) hint
 
 
 -- Get the least significant 3 bits for a register.
@@ -66,19 +217,6 @@ registerIndex r
     | elem r [CH, BPL, BP, EBP, RBP, R13B, R13W, R13D, R13] = [1, 0, 1]
     | elem r [DH, SIL, SI, ESI, RSI, R14B, R14W, R14D, R14] = [1, 1, 0]
     | elem r [BH, DIL, DI, EDI, RDI, R15B, R15W, R15D, R15] = [1, 1, 1]
-
-
--- The REX byte extension bit is 1 for extended registers and 0 for standard.
-rexBit :: Registers -> Int
-rexBit r
-    | elem r extendedRegisters = 1
-    | otherwise                = 0
-
-
--- Get the ModR/M bits for the given register. Returns 4 bits. The first bit is
--- the REX portion of the encoding. The last 3 bits are the ModR/M portion.
-encodeRegister :: Registers -> [Int]
-encodeRegister r = [rexBit r] ++ registerIndex r
 
 
 -- Get the operand directionality for an opcode. Controls the order in which
@@ -128,6 +266,11 @@ modRmByte :: [Operand] -> [Int]
 modRmByte [op1, op2] = modBits [op1, op2] ++ regBits op2 ++ rmBits op1
 
 
+-- Test if an opcode requires a ModR/M byte.
+needsModRm :: Int -> Bool
+needsModRm op = not (elem op [0x04, 0x05])
+
+
 -- Get an opcode based on an assembly command and its operators.
 opcode :: Command -> [Operand] -> Int
 opcode ADD [_,               Register r]
@@ -154,6 +297,7 @@ main = do
              source = "add byte al, bl",
              labels = [],
              command = ADD,
+             sizeHint = BYTE,
              operands = [
                  Register AL,
                  Register BL
@@ -163,6 +307,7 @@ main = do
              source = "add byte [rax], bl",
              labels = [],
              command = ADD,
+             sizeHint = BYTE,
              operands = [
                  Address RAX NoScale NoRegister NoDisplacement,
                  Register BL
@@ -172,6 +317,7 @@ main = do
              source = "add qword rax, rbx",
              labels = [],
              command = ADD,
+             sizeHint = QWORD,
              operands = [
                  Register RAX,
                  Register RBX
@@ -181,6 +327,7 @@ main = do
              source = "add qword [rax], rbx",
              labels = [],
              command = ADD,
+             sizeHint = QWORD,
              operands = [
                  Address RAX NoScale NoRegister NoDisplacement,
                  Register RBX
@@ -190,6 +337,7 @@ main = do
              source = "add bl, [rax]",
              labels = [],
              command = ADD,
+             sizeHint = NoSize,
              operands = [
                  Register BL,
                  Address RAX NoScale NoRegister NoDisplacement
@@ -199,6 +347,7 @@ main = do
              source = "add qword rbx, [rax]",
              labels = [],
              command = ADD,
+             sizeHint = QWORD,
              operands = [
                  Register RBX,
                  Address RAX NoScale NoRegister NoDisplacement
@@ -208,6 +357,7 @@ main = do
              source = "add byte al, 123",
              labels = [],
              command = ADD,
+             sizeHint = BYTE,
              operands = [
                  Register AL,
                  Immediate [123]
@@ -217,6 +367,7 @@ main = do
              source = "add dword eax, 123",
              labels = [],
              command = ADD,
+             sizeHint = DWORD,
              operands = [
                  Register EAX,
                  Immediate [123]
@@ -226,6 +377,7 @@ main = do
              source = "add byte ch, 123",
              labels = [],
              command = ADD,
+             sizeHint = BYTE,
              operands = [
                  Register CH,
                  Immediate [123]
@@ -235,6 +387,7 @@ main = do
              source = "add qword [rcx + 8 * rdx + 456], 456",
              labels = [],
              command = ADD,
+             sizeHint = QWORD,
              operands = [
                  Address RCX Scale8 RDX (Displacement32 456),
                  Immediate [0x01, 0xc8]
@@ -244,17 +397,66 @@ main = do
              source = "add rbx, 123",
              labels = [],
              command = ADD,
+             sizeHint = NoSize,
              operands = [
                  Register RBX,
                  Immediate [123]
              ]
+         },
+         Instruction {
+             source = "add ax, bx",
+             labels = [],
+             command = ADD,
+             sizeHint = NoSize,
+             operands = [
+                 Register AX,
+                 Register BX
+             ]
+         },
+         Instruction {
+             source = "add ax, r8w",
+             labels = [],
+             command = ADD,
+             sizeHint = NoSize,
+             operands = [
+                 Register AX,
+                 Register R8W
+             ]
          }]
+         --},
+         --Instruction {
+         --    source = "add [rbx], 123",
+         --    labels = [],
+         --    command = ADD,
+         --    sizeHint = NoSize,
+         --    operands = [
+         --        Address RBX NoScale NoRegister NoDisplacement,
+         --        Immediate [123]
+         --    ]
+         --},
+         --Instruction {
+         --    source = "add rbx, 123",
+         --    labels = [],
+         --    command = ADD,
+         --    sizeHint = DWORD,
+         --    operands = [
+         --        Register RBX,
+         --        Immediate [123]
+         --    ]
+         --}
 
     let go i = do
         let op = opcode (command i) (operands i)
+        let size = case opSize (sizeHint i) (operands i) of
+                  Success   s -> s
+                  Warn    m s -> trace m s
+                  Fail    m   -> error m
+        let prefix = sizePrefix op size (operands i)
         (showHex op " ") ++
+            (show size) ++ " " ++
             (show (modRmByte (encodedOrder op (operands i)))) ++
             "\t" ++
-            (source i)
+            (source i) ++ "\n" ++
+            (intercalate " " (map show prefix)) ++ "\n"
 
     putStrLn (intercalate "\n" (map go instructions))
