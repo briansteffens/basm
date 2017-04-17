5e8f84e4edd5cb3bd0d9792b1ea0e4fd98197606
Brian Steffens <briansteffens@gmail.com>
2017-04-17 02:20:46 +0000

    Add verbose flag

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.Environment
 import Data.List
+import Control.Monad
 import qualified Data.ByteString.Lazy as B
 
 import qualified Parser as P
@@ -17,27 +18,53 @@ chopExtension :: String -> String
 chopExtension fn = reverse (allAfterFirstDot (reverse fn))
 
 
+data Args = Args {
+    filename :: String,
+    verbose  :: Bool
+}
+
+
+parseArg :: String -> Args -> Args
+parseArg "-v" args = args { verbose  = True }
+parseArg fn   args = args { filename = fn }
+
+
+parseArgs' :: [String] -> Args -> Args
+parseArgs' []     args = args
+parseArgs' (a:as) args = parseArgs' as (parseArg a args)
+
+
+parseArgs :: [String] -> Args
+parseArgs as = parseArgs' as Args {
+    filename = [],
+    verbose = False
+}
+
+
 main :: IO ()
 main = do
-    args <- getArgs
+    argList <- getArgs
 
-    let filename = if length args == 1 then head args
-                                       else error("Usage: basm <filename>")
+    let args = if null argList then error("Usage: basm <filename>")
+                               else parseArgs argList
 
-    contents <- readFile filename
+    contents <- readFile (filename args)
 
     let (sections, directives, errors, parseDebug) = P.parse contents
 
-    putStrLn "basm lexer ---------------------------------------------------\n"
-    putStrLn parseDebug
-    putStrLn "\nbasm ast -----------------------------------------------------"
-    putStrLn ("\n" ++ (intercalate "\n\n" (map P.showCodeSection sections)))
-    putStrLn "\nbasm results -------------------------------------------------"
-    putStrLn ("\nerrors:\n" ++ (intercalate "\n" (map P.showError errors)))
+    let o = intercalate "\n" [
+           "basm lexer ---------------------------------------------------\n",
+           parseDebug,
+           "\nbasm ast -----------------------------------------------------",
+           ("\n" ++ (intercalate "\n\n" (map P.showCodeSection sections))),
+           "\nbasm results -------------------------------------------------",
+           ("\nerrors:\n" ++ (intercalate "\n" (map P.showError errors)))]
+
+    when (verbose args) $ putStrLn o
 
     let (bytes, debug) = E.assemble sections directives
 
     --putStrLn debug
 
-    let objFilename = (chopExtension filename) ++ ".o"
+    let objFilename = (chopExtension (filename args)) ++ ".o"
     B.writeFile objFilename bytes
